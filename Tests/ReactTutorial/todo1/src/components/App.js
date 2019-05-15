9c37b023b4091e8f08ea1f8c0f5e9c20f4d79fe6
kevin <kdt0116@tamu.edu>
2019-05-15 06:22:06 -0500

    here is an example of a controlled form 'value={this.state.firstName}'. it forces value to be the same as the state

--- Tests/ReactTutorial/todo1/src/components/App.js
+++ Tests/ReactTutorial/todo1/src/components/App.js
@@ -3,32 +3,31 @@ import Footer from "./Footer";
 import MainContent from "./MainContent";
 import Header from "./Header";
 
-
 class App extends React.Component {
-	constructor(){
+	constructor() {
 		super();
 		this.state = {
 			firstName: "",
 			loading: false,
-			character : {}
+			character: {}
 		};
-		this.handleChange = this.handleChange.bind(this)
-	};
+		this.handleChange = this.handleChange.bind(this);
+	}
 
 	//runs only once when the component mounts
-	componentDidMount(){
-		this.setState({loading: true})
+	componentDidMount() {
+		this.setState({ loading: true });
 		fetch("https://swapi.co/api/people/1")
 			.then(response => response.json())
 			.then(data => {
-				this.setState({character : data, loading: false})
-			})
+				this.setState({ character: data, loading: false });
+			});
 	}
 
-	handleChange(event){
+	handleChange(event) {
 		this.setState({
 			[event.target.name]: event.target.value
-		})
+		});
 	}
 
 	//runs everytime there is a state change or render is called
@@ -36,16 +35,30 @@ class App extends React.Component {
 		return (
 			<div>
 				<form>
-					<input type="text" name="firstName" placeholder="First Name" onChange={this.handleChange} />
-					<input type="text" name="lastName" placeholder="Last Name" onChange={this.handleChange} />
+					<input
+						type="text"
+						value={this.state.firstName}
+						name="firstName"
+						placeholder="First Name"
+						onChange={this.handleChange}
+					/>
+					<input
+						type="text"
+						value={this.state.lastName}
+						name="lastName"
+						placeholder="Last Name"
+						onChange={this.handleChange}
+					/>
 				</form>
-				<h1>{this.state.firstName} {this.state.lastName}</h1>
+				<h1>
+					{this.state.firstName} {this.state.lastName}
+				</h1>
 				<Header />
 				<MainContent />
 				<Footer />
 			</div>
 		);
-	};
+	}
 }
 
 export default App;
