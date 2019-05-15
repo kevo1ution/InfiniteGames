546c0e76e4cff49d097cc14f6d5b3f6ad4b3b43f
kevin <kdt0116@tamu.edu>
2019-05-15 06:48:32 -0500

    forms submit button setup

--- Tests/ReactTutorial/todo1/src/components/App.js
+++ Tests/ReactTutorial/todo1/src/components/App.js
@@ -12,9 +12,11 @@ class App extends React.Component {
 			loading: false,
 			isFriendly: true,
 			character: {},
-			gender: "default"
+			gender: "default",
+			favColor: "blue"
 		};
 		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
 	//runs only once when the component mounts
@@ -28,19 +30,24 @@ class App extends React.Component {
 	}
 
 	handleChange(event) {
+		console.log(event)
 		const { name, value, type, checked } = event.target;
 		this.setState({
 			[name]: type === "checkbox" ? checked : value
 		});
 	}
 
+	handleSubmit(event){
+		console.log(event);
+	}
+
 	//runs everytime there is a state change or render is called
 	render() {
 		return (
 			<div>
-				{/* Formik */}
-				<label>
-					<form>
+				<form onSubmit={this.handleSubmit}>
+					{/* Formik */}
+					<label>
 						<input
 							type="text"
 							value={this.state.firstName}
@@ -55,44 +62,60 @@ class App extends React.Component {
 							placeholder="Last Name"
 							onChange={this.handleChange}
 						/>
-					</form>
-				</label>
-				<label>
-					<textarea
-						name="textarea"
-						value={this.state.textarea}
+					</label>
+					<br />
+					<label>
+						<textarea
+							name="textarea"
+							value={this.state.textarea}
+							onChange={this.handleChange}
+						/>
+					</label>
+					<br />
+					<label>
+						<input
+							type="checkbox"
+							name="isFriendly"
+							checked={this.state.isFriendly}
+							onChange={this.handleChange}
+						/>{" "}
+						Is friend?
+					</label>
+					<br />
+					<label>
+						<input
+							type="radio"
+							name="gender"
+							value="male"
+							checked={this.state.gender === "male"}
+							onChange={this.handleChange}
+						/>{" "}
+						Male?
+					</label>
+					<br />
+					<label>
+						<input
+							type="radio"
+							name="gender"
+							value="female"
+							checked={this.state.gender === "female"}
+							onChange={this.handleChange}
+						/>{" "}
+						female?
+					</label>
+					<br />
+					<label>Favorite Color:</label>
+					<select
+						value={this.state.favColor}
+						name="favColor"
 						onChange={this.handleChange}
-					/>
-				</label>
-				<br />
-				<label>
-					<input
-						type="checkbox"
-						name="isFriendly"
-						checked={this.state.isFriendly}
-						onChange={this.handleChange}
-					/> Is friend? 
-				</label>
-				<br />
-				<label>
-					<input
-						type="radio"
-						name="gender"
-						value="male"
-						checked={this.state.gender === "male"}
-						onChange={this.handleChange}
-					/> Male? 
-				</label>
-				<br />
-				<label>
-					<input
-						type="radio"
-						name="gender"
-						value="female"
-						checked={this.state.gender === "female"}
-						onChange={this.handleChange}
-					/> female? 
-				</label>
+					>
+						<option value="blue">Blue</option>
+						<option value="red">Red</option>
+						<option value="green">Green</option>
+					</select>
+					<button>Submit</button>
+				</form>
 
 				<h1>
 					{this.state.firstName} {this.state.lastName}
