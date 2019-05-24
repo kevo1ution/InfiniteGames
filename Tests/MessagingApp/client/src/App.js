11462766fc33fcb5f00a3edcd62407b48ed0617d
kevin <kdt0116@tamu.edu>
2019-05-24 01:04:02 -0500

    added message component

--- Tests/MessagingApp/client/src/App.js
+++ Tests/MessagingApp/client/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
-import Customers from "./components/customers/customers";
+import Message from "./components/message/message"
 
 import io from 'socket.io-client';
 const socket = io.connect('http://localhost:5000');
 
 socket.on('messages', function(data){
-  alert(data.hello);
-})
+});
 
 class App extends React.Component {
 	constructor() {
