df45dc581fb7321574d90cb3d3bc9c0f8c29af64
tingtongtang <kdt0116@tamu.edu>
2019-07-27 15:13:39 -0500

    testing server configuration to have multiple environments

--- server/server.js
+++ server/server.js
@@ -1,6 +1,21 @@
-
 //dependencies
-const express = require('express');
+const express = require("express");
 
+//environment varialbes
+process.env.NODE_ENV = "development";
+
+//config variables
+const config = require("./config/config.js");
+
+//module variables
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json(global.gConfig);
+});
+
+app.listen(global.gConfig.node_port, () => {
+  console.log(
+    `${global.gConfig.app_name} listening on port ${global.gConfig.node_port}`
+  );
+});
