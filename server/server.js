c55e7aafcf4b9acef5c8d926b721bf73dda2076b
tingtongtang <kdt0116@tamu.edu>
2019-07-27 22:05:38 -0500

    messing around with graphql, and graphiql interface

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 //dependencies
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 //environment varialbes
 process.env.NODE_ENV = "development";
@@ -10,10 +12,30 @@ const config = require("./config/config.js");
 //module variables
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json(global.gConfig);
-});
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
 
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return "Hello world!";
+  }
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+
+//start up the server and display configuraitons
 app.listen(global.gConfig.node_port, () => {
   console.log(
     `${global.gConfig.app_name} listening on port ${global.gConfig.node_port}`
